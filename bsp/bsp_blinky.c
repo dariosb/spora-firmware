cb2a19ec72dfc34ef27574164ffe7f151e7e9197
Dario Baliña <db@vortexmakes.com>
2018-01-23 10:05:39 -0300

    RKH blinky demo of FRDMKL03

---
+++ bsp/bsp_blinky.c
@@ -0,0 +1,122 @@
+/*
+ *  --------------------------------------------------------------------------
+ *
+ *                                Framework RKH
+ *                                -------------
+ *
+ *            State-machine framework for reactive embedded systems
+ *
+ *                      Copyright (C) 2010 Leandro Francucci.
+ *          All rights reserved. Protected by international copyright laws.
+ *
+ *
+ *  RKH is free software: you can redistribute it and/or modify it under the
+ *  terms of the GNU General Public License as published by the Free Software
+ *  Foundation, either version 3 of the License, or (at your option) any
+ *  later version.
+ *
+ *  RKH is distributed in the hope that it will be useful, but WITHOUT ANY
+ *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ *  more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with RKH, see copying.txt file.
+ *
+ *  Contact information:
+ *  RKH web site:   http://sourceforge.net/projects/rkh-reactivesys/
+ *  e-mail:         francuccilea@gmail.com
+ *  ---------------------------------------------------------------------------
+ */
+
+/**
+ *  \file       trace_io.c
+ *  \brief      Socket COM Port support for DemoQE128-S08
+ *
+ *  \ingroup    bsp
+ */
+
+/* -------------------------- Development history -------------------------- */
+/*
+ *  2017.04.14  DaBa  v2.4.05  Initial version
+ */
+
+/* -------------------------------- Authors -------------------------------- */
+/*
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
+ *  DaBa  Dario Baliña       dariosb@gmail.com
+ */
+/* --------------------------------- Notes --------------------------------- */
+/* ----------------------------- Include files ----------------------------- */
+#include "blinky.h"
+#include "rkh.h"
+#include "bsp.h"
+//#include "mcu.h"
+//#include "gpio.h"
+//#include "serial.h"
+
+#include "pin_mux.h"
+#include "bsp.h"
+#include "i2c.h"
+
+/* ----------------------------- Local macros ------------------------------ */
+/* ------------------------------- Constants ------------------------------- */
+/* ---------------------------- Local data types --------------------------- */
+/* ---------------------------- Global variables --------------------------- */
+/* ---------------------------- Local variables ---------------------------- */
+/* ----------------------- Local function prototypes ----------------------- */
+/* ---------------------------- Local functions ---------------------------- */
+void
+SysTick_Handler(void)
+{
+	RKH_TIM_TICK(0);
+}
+
+/* ---------------------------- Global functions --------------------------- */
+void
+bsp_init(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+
+    gpio_pin_config_t led_config = {kGPIO_DigitalOutput, 0};
+
+     BOARD_InitPins();
+     BOARD_BootClockRUN();
+
+     SysTick_Config(SystemCoreClock/RKH_CFG_FWK_TICK_RATE_HZ);
+
+     I2C_releaseBus();
+     BOARD_I2C_ConfigurePins();
+
+     GPIO_PinInit(BOARD_LED_RED_GPIO, BOARD_LED_RED_GPIO_PIN, &led_config);
+     LED_RED_TOGGLE();
+
+
+    rkh_fwk_init();
+
+    RKH_FILTER_ON_GROUP(RKH_TRC_ALL_GROUPS);
+    RKH_FILTER_ON_EVENT(RKH_TRC_ALL_EVENTS);
+    RKH_FILTER_OFF_EVENT(RKH_TE_TMR_TOUT);
+    RKH_FILTER_OFF_EVENT(RKH_TE_SM_STATE);
+    RKH_FILTER_OFF_SMA(blinky);
+    RKH_FILTER_OFF_ALL_SIGNALS();
+
+    RKH_TRC_OPEN();
+
+    RKH_ENA_INTERRUPT();
+}
+
+void
+bsp_ledOn(void)
+{
+	LED_RED_ON();
+}
+
+void
+bsp_ledOff(void)
+{
+	LED_RED_OFF();
+}
+
+/* ------------------------------ End of file ------------------------------ */
--- test/rkhOnFRDM/main.c
+++ test/rkhOnFRDM/main.c
@@ -1,101 +1,46 @@
-/*
- *  --------------------------------------------------------------------------
- *
- *                                    Spora
- *                                -------------
- *
- *                  Coin-sized programmable multi-sensor node
- *
- *                      Copyright (C) 2018  Enye Tech.
- *          All rights reserved. Protected by international copyright laws.
- *
- *
- *  Spora is an open-source, programmable multi-sensor node,
- *  specifically designed for wearables but with endless opportunities.
- *
- *  Spora is free software: you can redistribute it and/or modify it under the
- *  terms of the GNU General Public License as published by the Free Software
- *  Foundation, either version 3 of the License, or (at your option) any
- *  later version.
- *
- *  Spora is distributed in the hope that it will be useful, but WITHOUT ANY
- *  WARRANTY; without even the implied warranty of MERCHANTABILITY or
- *  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- *  more details.
- *
- *  You should have received a copy of the GNU General Public License along
- *  with RKH, see copying.txt file.
- *
- *  Contact information:
- *  Spora web site: http://www.sporaio.com
- *  e-mail:         hello@sporaio.com
- *  ---------------------------------------------------------------------------
- */
-
 /**
  *  \file       main.c
- *  \brief      Simple test app for Spora BSP on FRDMKL03.
- *
- *  \ingroup    bsp_test
+ *  \brief      Example application.
  */
 
 /* -------------------------- Development history -------------------------- */
 /*
- *  2018.01.5  DaBa  v0.0.01  Initial version
+ *  2016.03.17  LeFr  v1.0.00  Initial version
  */
 
 /* -------------------------------- Authors -------------------------------- */
 /*
- *  DaBa  Dario Baliña       db@vortexmakes.com
+ *  LeFr  Leandro Francucci  francuccilea@gmail.com
  */
 
 /* --------------------------------- Notes --------------------------------- */
 /* ----------------------------- Include files ----------------------------- */
-#include "pin_mux.h"
+#include "rkh.h"
+#include "blinky.h"
 #include "bsp.h"
-#include "i2c.h"
-#include "mpu9250.h"
-#include "pushbutton.h"
-#include "spora.h"
-#include "codeless.h"
+
 
 /* ----------------------------- Local macros ------------------------------ */
+#define QSTO_SIZE           4
+
 /* ------------------------------- Constants ------------------------------- */
 /* ---------------------------- Local data types --------------------------- */
 /* ---------------------------- Global variables --------------------------- */
 /* ---------------------------- Local variables ---------------------------- */
-void
-codelessHook(char *p)
-{
-
-}
+static RKH_EVT_T *qsto[QSTO_SIZE];
 
 /* ----------------------- Local function prototypes ----------------------- */
 /* ---------------------------- Local functions ---------------------------- */
 /* ---------------------------- Global functions --------------------------- */
 int
-main(void)
+main(int argc, char *argv[])
 {
-    gpio_pin_config_t led_config = {kGPIO_DigitalOutput, 0};
+    bsp_init(argc, argv);
 
-    BOARD_InitPins();
-    BOARD_BootClockRUN();
+    RKH_SMA_ACTIVATE(blinky, qsto, QSTO_SIZE, 0, 0);
+    rkh_fwk_enter();
 
-    I2C_releaseBus();
-    BOARD_I2C_ConfigurePins();
-
-    GPIO_PinInit(BOARD_LED_RED_GPIO, BOARD_LED_RED_GPIO_PIN, &led_config);
-    LED_RED_TOGGLE();
-
-    codeless_init(codelessHook);
-    if (mpu9250_init() == true)
-    {
-        pushbutton_init();
-        while (1)
-        {
-            spora_task();
-        }
-    }
+    RKH_TRC_CLOSE();
     return 0;
 }
 
